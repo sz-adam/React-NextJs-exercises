26445f0e13e1ecbfa33a0493cb12d8ef84254f71
Szabó Ádám <szaboadam21@gmail.com>
2024-10-23 16:16:19 +0200

    navigating-programmatically

--- React-Router/src/pages/Home.jsx
+++ React-Router/src/pages/Home.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { useNavigate } from "react-router-dom";
 
 function HomePage() {
+  const navigate = useNavigate();
+  function navigateHandler() {
+    navigate('/products')
+  }
+
   return (
-    <div>HomePage</div>
-  )
+    <div>
+      <h1>HomePage</h1>
+      <button onClick={navigateHandler}>Navigate</button>
+    </div>
+  );
 }
 
-export default HomePage
+export default HomePage;
