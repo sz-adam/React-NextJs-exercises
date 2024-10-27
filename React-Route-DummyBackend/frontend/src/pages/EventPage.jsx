536c87fcb0ecb3623ee980d3ab59b0b0675cf237
Szabó Ádám <szaboadam21@gmail.com>
2024-10-27 11:10:30 +0100

    only deferring the loading of the retrieved data

--- React-Route-DummyBackend/frontend/src/pages/EventPage.jsx
+++ React-Route-DummyBackend/frontend/src/pages/EventPage.jsx
@@ -1,18 +1,28 @@
+import { Suspense } from "react";
 import EventsList from "../components/EventsList";
-import { json, useLoaderData } from "react-router-dom";
+import { Await, defer, json, useLoaderData } from "react-router-dom";
 
 function EventsPage() {
-  const data = useLoaderData();
-  const events = data.events;
+  // const data = useLoaderData();
+  // const events = data.events;
   //if(data.isError){
   //  return <p>{data.message}</p>
   //}
-  return <EventsList events={events} />;
+  //return <EventsList events={events} />;
+  const { events } = useLoaderData();
+
+  return (
+    <Suspense fallback={<p style={{ textAlign: "center" }}>Loading...</p>}>
+      <Await resolve={events}>
+        {(loadedEvents) => <EventsList events={loadedEvents} />}
+      </Await>
+    </Suspense>
+  );
 }
 
 export default EventsPage;
 
-export async function loader() {
+async function loadEvents() {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
@@ -23,6 +33,14 @@ export async function loader() {
     //  });
     return json({ message: "Could not fetch events." }), { status: 500 };
   } else {
-    return response;
+    // return response;
+    const resData = await response.json();
+    return resData.events;
   }
+}
+
+export function loader() {
+  return defer({
+    events: loadEvents(),
+  });
 }
