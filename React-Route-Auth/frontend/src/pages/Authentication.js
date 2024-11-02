2f4a30de405d68f5f16e3564945f57616a004324
Szabó Ádám <szaboadam21@gmail.com>
2024-11-02 11:05:12 +0100

    to use, create, edit and delete tokens

--- React-Route-Auth/frontend/src/pages/Authentication.js
+++ React-Route-Auth/frontend/src/pages/Authentication.js
@@ -38,6 +38,11 @@ export async function action({ request }) {
     throw json({ message: "Could not authenticate user." }, { status: 500 });
   }
 
+  const resData = await response.json();
+  const token = resData.token;
+  
+  localStorage.setItem('token', token)
+
   // soon: manage that token
   return redirect("/");
 }
