e827b5e33b9b1a12b443d86898275ba70bcbff92
Szabó Ádám <szaboadam21@gmail.com>
2024-11-05 19:03:47 +0100

    the search can also be started with the enter key

--- MovieDB/src/components/Search.tsx
+++ MovieDB/src/components/Search.tsx
@@ -16,6 +16,13 @@ const Search = ({ onSearch }: Props) => {
         onSearch(inputValue);
     };
 
+    // Enter leütésének kezelése
+    const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === 'Enter') {
+            handleSearchClick();
+        }
+    };
+
     return (
         <div className="md:w-full max-w-sm min-w-[200px]">
             <div className="relative">
@@ -24,9 +31,10 @@ const Search = ({ onSearch }: Props) => {
                     onChange={handleInputChange}
                     className="w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md pl-3 pr-28 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
                     placeholder="Search..."
+                    onKeyDown={handleKeyDown}
                 />
                 <button
-                    onClick={handleSearchClick}
+                    onClick={handleSearchClick}                   
                     className="absolute top-1 right-1 flex items-center rounded bg-slate-800 py-1 px-2.5 border border-transparent text-center text-sm text-white transition-all shadow-sm hover:shadow focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
                     type="button"
                 >
