0079733ac4b967badda22c0bb94f2286be6f9687
Szabó Ádám <112242221+sz-adam@users.noreply.github.com>
2024-12-14 20:31:12 +0100

    pages test

--- tetris-game/vite.config.ts
+++ tetris-game/vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/React-NextJs-exercises/tetris-game/",
   plugins: [react()],
-})
+});
