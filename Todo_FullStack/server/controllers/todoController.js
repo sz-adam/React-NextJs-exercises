793d12ad7e111c607276970c52ccdd12a205fb74
Szabó Ádám <112242221+sz-adam@users.noreply.github.com>
2025-01-05 16:25:35 +0100

    post , get, put , delete function

--- Todo_FullStack/server/controllers/todoController.js
+++ Todo_FullStack/server/controllers/todoController.js
@@ -1,19 +1,71 @@
+const { PrismaClient } = require("@prisma/client");
+const handleError = require("./handleError");
+
+const prisma = new PrismaClient();
+
 
 const getTodos = async (req, res) => {
- console.log("összes todo")
+  try {
+    const todos = await prisma.todo.findMany();
+    res.status(200).json(todos);
+  } catch (error) {
+    handleError(res, error, "Error fetching todos");
+  } finally {
+    await prisma.$disconnect();
+  }
 };
 
 const createTodo = async (req, res) => {
-   console.log("todo létrehozása");
-
+  const { title, description, priority } = req.body;
+  try {
+    const newTodo = await prisma.todo.create({
+      data: {
+        title,
+        description,
+        priority,
+      },
+    });
+    res.status(201).json(newTodo);
+  } catch (error) {
+    handleError(res, error, "Error creating TODO");
+  }
 };
 
 const updateTodo = async (req, res) => {
- console.log("todo módosítása");
+  const { id } = req.params;
+  const { title, description, priority, completed } = req.body;
+
+  try {
+    const updatedTodo = await prisma.todo.update({
+      where: { id: parseInt(id) },
+      data: {
+        title,
+        description,
+        priority,
+        completed,
+      },
+    });
+
+    res.status(200).json(updatedTodo);
+  } catch (error) {
+    handleError(res, error, "Error updating todo");
+  } finally {
+    await prisma.$disconnect();
+  }
 };
 
 const deleteTodo = async (req, res) => {
- console.log("todo törlése");
+  const { id } = req.params;
+  try {
+    const deletedTodo = await prisma.todo.delete({
+      where: { id: parseInt(id) },
+    });
+    res.status(200).json({ message: "Todo deleted successfully", deletedTodo });
+  } catch (error) {
+    handleError(res, error, "Error deleting todo");
+  } finally {
+    await prisma.$disconnect();
+  }
 };
 
 module.exports = {
