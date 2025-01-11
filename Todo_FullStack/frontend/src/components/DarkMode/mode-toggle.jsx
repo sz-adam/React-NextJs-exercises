500f3e286cee46867ba821d7334d30ccf94192f7
Szabó Ádám <112242221+sz-adam@users.noreply.github.com>
2025-01-11 13:52:25 +0100

    add dark mode

---
+++ Todo_FullStack/frontend/src/components/DarkMode/mode-toggle.jsx
@@ -0,0 +1,22 @@
+import { Moon, Sun } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { useTheme } from "@/context/themeContext";
+
+export function ModeToggle() {
+  const { theme, setTheme } = useTheme();
+
+  const toggleTheme = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+
+  return (
+    <Button variant="default" size="icon" onClick={toggleTheme}>
+      {theme === "dark" ? (
+        <Sun className="h-5 w-5" />
+      ) : (
+        <Moon className="h-5 w-5" />
+      )}
+      <span className="sr-only">Toggle theme</span>
+    </Button>
+  );
+}
