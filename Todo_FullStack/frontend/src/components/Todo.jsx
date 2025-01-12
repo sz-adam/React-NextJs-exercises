26182c60571c67caf158204c6a9522af32c4f618
Szabó Ádám <112242221+sz-adam@users.noreply.github.com>
2025-01-12 12:10:51 +0100

    get todos

--- Todo_FullStack/frontend/src/components/Todo.jsx
+++ Todo_FullStack/frontend/src/components/Todo.jsx
@@ -1,82 +1,11 @@
-import React, { useState } from "react";
 import TodoCard from "./TodoCard";
+import { useTodos } from "@/context/TodoContext";
 
 function Todo() {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      title: "Finish React project",
-      description: "Complete all sections of the React tutorial",
-      priority: "High",
-      completed: false,
-      category: "Work",
-      dueDate: new Date(),     
-    },
-    {
-      id: 2,
-      title: "Buy groceries",
-      description: "Milk, eggs, bread, and fruits",
-      priority: "Medium",
-      completed: false,
-      category: "Shopping",
-      dueDate: new Date(),
-     
-    },
-    {
-      id: 3,
-      title: "Exercise at the gym",
-      description: "Full body workout",
-      priority: "Low",
-      completed: true,
-      category: "Fitness",
-      dueDate: new Date(),
-     
-    },
-    {
-      id: 4,
-      title: "Finish React project",
-      description: "Complete all sections of the React tutorial",
-      priority: "High",
-      completed: false,
-      category: "Work",
-      dueDate: new Date(),
-     
-    },
-    {
-      id: 5,
-      title: "Buy groceries",
-      description: "Milk, eggs, bread, and fruits",
-      priority: "Medium",
-      completed: false,
-      category: "Shopping",
-      dueDate: new Date(),
-     
-    },
-    {
-      id: 6,
-      title: "Exercise at the gym",
-      description: "Full body workout",
-      priority: "Low",
-      completed: true,
-      category: "Fitness",
-      dueDate: new Date(),
-     
-    },
-    {
-      id: 7,
-      title: "Exercise at the gym",
-      description: "Full body workout",
-      priority: "Low",
-      completed: true,
-      category: "Fitness",
-      dueDate: new Date(),
-     
-    },
-  ]);
+  const { todos } = useTodos();
 
   return (
     <div className="lg:order-1 w-full lg:w-3/5  p-4 mx-auto  rounded-lg shadow-lg">
-      {/* Todo lista */}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {todos.map((todo) => (
           <TodoCard key={todo.id} todo={todo} />
