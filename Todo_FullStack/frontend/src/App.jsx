22e7275d2d56e2ce61c2f8519e0e5d337c21d3eb
Szabó Ádám <112242221+sz-adam@users.noreply.github.com>
2025-01-12 11:36:49 +0100

    create context

--- Todo_FullStack/frontend/src/App.jsx
+++ Todo_FullStack/frontend/src/App.jsx
@@ -4,16 +4,19 @@ import Header from "./components/Header";
 import { Category } from "./components/Category";
 import Todo from "./components/Todo";
 import CalendarTodo from "./components/CalendarTodo";
+import { TodoProvider } from "./context/todoContext";
 
 function App() {
   return (
     <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
-      <Header />
-      <div className="flex flex-col lg:flex-row gap-4">
-        <Category />
-        <CalendarTodo />
-        <Todo />
-      </div>
+      <TodoProvider>
+        <Header />
+        <div className="flex flex-col lg:flex-row gap-4">
+          <Category />
+          <CalendarTodo />
+          <Todo />
+        </div>
+      </TodoProvider>
     </ThemeProvider>
   );
 }
