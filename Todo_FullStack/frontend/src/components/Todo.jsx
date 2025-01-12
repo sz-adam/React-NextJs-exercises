85c12390ca6dd8823b782963fb33d02ea074445c
Szabó Ádám <112242221+sz-adam@users.noreply.github.com>
2025-01-12 16:19:29 +0100

    todo reverse

--- Todo_FullStack/frontend/src/components/Todo.jsx
+++ Todo_FullStack/frontend/src/components/Todo.jsx
@@ -7,7 +7,7 @@ function Todo() {
   return (
     <div className="lg:order-1 w-full lg:w-3/5  p-4 mx-auto  rounded-lg shadow-lg">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {todos.map((todo) => (
+        {[...todos].reverse().map((todo) => (
           <TodoCard key={todo.id} todo={todo} />
         ))}
       </div>
