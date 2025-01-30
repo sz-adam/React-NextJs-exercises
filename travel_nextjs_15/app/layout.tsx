1b3caf50d8fd54026c952190ed573f9db6da341a
Szabó Ádám <112242221+sz-adam@users.noreply.github.com>
2025-01-30 18:31:16 +0100

    add navigation

--- travel_nextjs_15/app/layout.tsx
+++ travel_nextjs_15/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 import { Poppins } from "next/font/google";
+import ResponsiveNav from "../components/Home/Navbar/ResponsiveNav";
 
 const font = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
@@ -20,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={` ${font.className} antialiased`}>{children}</body>
+      <body className={` ${font.className} antialiased`}><ResponsiveNav />{children}</body>
     </html>
   );
 }
---
+++ travel_nextjs_15/components/Home/Navbar/Nav.tsx
@@ -0,0 +1,73 @@
+"use client";
+import { navLinks } from "@/constant/constant";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import { HiBars3BottomRight } from "react-icons/hi2";
+import { TbAirBalloon } from "react-icons/tb";
+
+type Props = {
+  openNav: () => void;
+};
+
+const Nav = ({ openNav }: Props) => {
+  const [navBg, setNavbg] = useState(false);
+  useEffect(() => {
+    const handler = () => {
+      if (window.scrollY >= 90) setNavbg(true);
+      if (window.scrollY <= 90) setNavbg(false);
+    };
+    window.addEventListener("scroll", handler);
+    return () => window.addEventListener("scroll", handler);
+  }, []);
+  return (
+    <div
+      className={` ${
+        navBg ? "bg-blue-950 shadow-md" : "fixed"
+      } transition-all duration-200 h-[12vh] z-[1000] fixed w-full`}
+    >
+      <div className="flex items-center h-full justify-between w-[90%] xl:w-[80%] mx-auto">
+        {/**LOGO */}
+        <div className="flex items-center space-x-2">
+          <div className="w-10 h-10 bg-rose-500 rounded-full flex items-center justify-center flex-col">
+            <TbAirBalloon className="w-6 h-6 text-white" />
+          </div>
+          <h1 className="text-xl md:text-2xl text-white uppercase font-bold">
+            Tripi
+          </h1>
+        </div>
+        {/**NavLinks */}
+        <div className="hidden lg:flex items-center space-x-10">
+          {navLinks.map((link) => {
+            return (
+              <Link href={link.url} key={link.id}>
+                <p
+                  className="relative text-white text-base font-medium w-fit block after:block after:content-[''] 
+                  after:absolute after:h-[3px] after:bg-yellow-300 after:w-full after:scale-x-0
+                  after:hover:scale-x-100 after:transition duration-300 after:origin-right"
+                >
+                  {link.label}
+                </p>
+              </Link>
+            );
+          })}
+        </div>
+        {/**buttons */}
+        <div className="flex items-center space-x-4">
+          <button
+            className="md:px-12 md:py-2.5 px-8 py-2 text-black text-base
+           bg-white hover:bg-gray-300 transition-all duration-200 rounded-lg"
+          >
+            Book Now
+          </button>
+          {/**burger menu */}
+          <HiBars3BottomRight
+            onClick={openNav}
+            className="w-8 h-8 cursor-pointer text-white lg:hidden"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
