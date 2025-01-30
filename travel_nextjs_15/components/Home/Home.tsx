ea403fd98ef655d1b978d561797a38ffe9858f4e
Szabó Ádám <112242221+sz-adam@users.noreply.github.com>
2025-01-30 19:19:12 +0100

    hero

--- travel_nextjs_15/components/Home/Home.tsx
+++ travel_nextjs_15/components/Home/Home.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import Hero from "./Hero/Hero";
 
 const Home = () => {
   return (
-    <div className='overflow-hidden h-[10000px]'>Home</div>
-  )
-}
+    <div className="overflow-hidden">
+      <Hero />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
