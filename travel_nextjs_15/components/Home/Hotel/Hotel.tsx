4bf601460b457631a252a031a238a7643ad67366
Szabó Ádám <112242221+sz-adam@users.noreply.github.com>
2025-01-31 19:49:36 +0100

    Hotel card

---
+++ travel_nextjs_15/components/Home/Hotel/Hotel.tsx
@@ -0,0 +1,25 @@
+import SectionHeading from "@/components/Helper/SectionHeading";
+import { hotelsData } from "@/data/data";
+import React from "react";
+import HotelCard from "./HotelCard";
+
+const Hotel = () => {
+  return (
+    <div className="pt-20 pb-20">
+      {/*Section Heading*/}
+      <SectionHeading heading="Recommended Hotels" />
+      <div className="e-[80%] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 items-center mt-16">
+        {/**Hotel Card */}
+        {hotelsData.map((data) => {
+          return (
+            <div key={data.id}>
+              <HotelCard hotel={data}/>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Hotel;
