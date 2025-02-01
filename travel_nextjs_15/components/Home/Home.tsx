a17cb3a863d99a829beb65e84a88d74b2358f174
Szabó Ádám <112242221+sz-adam@users.noreply.github.com>
2025-02-01 19:59:06 +0100

    choose and review

--- travel_nextjs_15/components/Home/Home.tsx
+++ travel_nextjs_15/components/Home/Home.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import Hero from "./Hero/Hero";
 import Destination from "./Destination/Destination";
 import Hotel from "./Hotel/Hotel";
+import WhyChoose from "./WhyChoose/WhyChoose";
+import Reiview from "./Review/Reiview";
 
 const Home = () => {
   return (
@@ -9,6 +11,8 @@ const Home = () => {
       <Hero />
       <Destination />
       <Hotel />
+      <WhyChoose />
+      <Reiview />
     </div>
   );
 };
